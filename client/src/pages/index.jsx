79afc0cf2d8b2d13bebad980ed73c5735a338e55
Vrishabhshri <vrishabhshri@gmail.com>
2024-05-25 14:43:02 -0400

    Added home page from old template

---
+++ client/src/pages/index.jsx
@@ -0,0 +1,50 @@
+import "../styles/universal.css";
+import "../styles/index.css";
+import React from 'react';
+
+export default function Home() {
+
+    return (
+
+        <div id="main">
+
+            <div id="title-bar">
+
+                <div id="title">
+
+                    Multi-Wrdle
+
+                </div>
+
+            </div>
+
+            <div id="how-to-play-button">
+
+                How to Play
+
+            </div>
+
+            <div className="instructions">
+
+                The game is like world and other instructions will go here...
+
+            </div>
+            
+            <button id="play-button">Play</button>
+
+            <div id="enter-code">
+
+                Or enter a code
+
+            </div>
+
+            <input id="enter-code-input" type="text"/>
+
+            <a href="guesser-board.html">Guesser</a>
+            <a href="chooser-board.html">Chooser</a>
+
+        </div>
+
+    );
+
+}
