2e105ca414cafbbb2d420035df622b14a7a5fdac
Vrishabhshri <vrishabhshri@gmail.com>
2024-05-25 14:53:06 -0400

    Added waiting-room from old template

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+// Importing router
+
+import {
+  createBrowserRouter,
+  RouterProvider
+} from "react-router-dom";
+
 // Importing pages
 
 import Home from "./pages/home.jsx";
+import WaitingRoom from './pages/waiting-room.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/waiting-room",
+    element: <WaitingRoom />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
---
+++ client/src/pages/waiting-room.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import "../styles/waiting-room.css";
+
+function WaitingRoom() {
+  return (
+
+    <div id="main">
+
+        <div id="code-title">
+            
+            Your code is:
+
+        </div>
+
+        <div id="code">
+
+            
+
+        </div>
+
+        <div id="players-box">
+
+            
+
+        </div>
+
+    </div>
+
+  );
+
+}
+
+export default WaitingRoom;
