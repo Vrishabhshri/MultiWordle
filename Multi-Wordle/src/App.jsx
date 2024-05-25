005b8450132d63f33b57845156672b3789067d61
Vrishabhshri <vrishabhshri@gmail.com>
2024-05-25 12:11:05 -0400

    Importing html from old project

--- Multi-Wordle/src/App.jsx
+++ Multi-Wordle/src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+export default function App() {
 
-function App() {
-  const [count, setCount] = useState(0)
+  return <div id="main">
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div id="title-bar">
+
+        <div id="title">
+
+            Multi-Wrdle
+
+        </div>
+
+    </div>
+
+    <div id="how-to-play-button">
+
+        How to Play
+
+    </div>
+
+    <div className="instructions">
+
+        The game is like world and other instructions will go here...
+
+    </div>
+    
+    <button id="play-button">Play</button>
+
+    <div id="enter-code">
+
+        Or enter a code
+
+    </div>
+
+    <input id="enter-code-input" type="text"/>
+
+    <a href="guesser-board.html">Guesser</a>
+    <a href="chooser-board.html">Chooser</a>
+
+  </div>
+
 }
-
-export default App
